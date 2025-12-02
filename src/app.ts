0a0852dc7fe2e6e5e5b1f616ad163c5a690ee3ff
rangetik <dobroiranget@gmail.com>
2025-12-02 23:18:09 +0200

    added creating, deleting, editing reviews

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,37 @@
-import express from 'express';
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
-import cors from 'cors';
-import compression from 'compression';
-import quizRouter from './routers/quizRouter.js';
-import authRouter from './routers/authRouter.js';
-import userRouter from './routers/userRouter.js';
-import globalErrorHandler from './controllers/errorController.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+import quizRouter from "./routers/quizRouter.js";
+import authRouter from "./routers/authRouter.js";
+import userRouter from "./routers/userRouter.js";
+import globalErrorHandler from "./controllers/errorController.js";
+import cookieParser from "cookie-parser";
+import reviewRouter from "./routers/reviewRouter.js";
 export const app = express();
-app.set('query parser', 'extended');
+app.set("query parser", "extended");
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 app.use(cors());
 app.use(helmet());
 const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour!'
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
 });
 
-app.use('/api', limiter);
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use("/api", limiter);
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(compression());
 app.use(cookieParser());
 
-app.use('/api/quizzes', quizRouter);
-app.use('/api/users', authRouter);
-app.use('/api/user-profiles', userRouter);
+app.use("/api/quizzes", quizRouter);
+app.use("/api/users", authRouter);
+app.use("/api/user-profiles", userRouter);
+
+app.use("/api/quizzes/:quizId/reviews", reviewRouter);
+app.use("/api/reviews", reviewRouter);
 
 app.use(globalErrorHandler);
-
-
-
-
-
-
---
+++ src/routers/reviewRouter.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import * as reviewController from "../controllers/reviewController.js";
+import { protect } from "../controllers/authCotroller.js";
+
+const router = express.Router({ mergeParams: true });
+
+router
+  .route("/")
+  .get(reviewController.getQuizReviews)
+  .post(protect, reviewController.createReview);
+
+router
+  .route("/:id")
+  .delete(protect, reviewController.deleteReview)
+  .patch(protect, reviewController.updateReview);
+
+export default router;
