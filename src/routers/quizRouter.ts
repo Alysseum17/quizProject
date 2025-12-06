41efec8779b3746579c446d215e3f25d312aedbd
rangetik <dobroiranget@gmail.com>
2025-12-06 15:00:09 +0200

    questions: wire up question routes(delegations)

--- src/routers/quizRouter.ts
+++ src/routers/quizRouter.ts
@@ -1,17 +1,20 @@
-import express from 'express';
-import * as quizController from '../controllers/quizController.js';
-import {protect} from '../controllers/authCotroller.js';
+import express from "express";
+import * as quizController from "../controllers/quizController.js";
+import { protect } from "../controllers/authCotroller.js";
+import questionRouter from "./questionRouter.js";
 
 const router = express.Router();
 
-router.get('/', quizController.getAllQuiz);
-router.get('/rating', quizController.getSortedQuizByRating);
-router.get('/name/:name', quizController.findQuizByName);
-router.get('/:id', quizController.findQuizById);
-router.post('/', protect, quizController.createQuiz);
-router.post('/complex', protect, quizController.createQuizComplex);
-router.post('/:quizId/start', protect, quizController.startQuizAttempt);
-router.put('/:id', quizController.updateQuiz);
-router.delete('/:id', quizController.softDeleteQuiz);
+router.use("/:quizId/questions", questionRouter);
+
+router.get("/", quizController.getAllQuiz);
+router.get("/rating", quizController.getSortedQuizByRating);
+router.get("/name/:name", quizController.findQuizByName);
+router.get("/:id", quizController.findQuizById);
+router.post("/", protect, quizController.createQuiz);
+router.post("/complex", protect, quizController.createQuizComplex);
+router.post("/:quizId/start", protect, quizController.startQuizAttempt);
+router.put("/:id", quizController.updateQuiz);
+router.delete("/:id", quizController.softDeleteQuiz);
 
 export default router;
